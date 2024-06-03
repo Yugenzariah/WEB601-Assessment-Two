762b3418d2eabf86e32d3db3f255c7e4980ad74d
Azariah Keith <yugenzariah@gmail.com>
2024-06-03 23:05:48 +1200

    Created API routings

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+// Use middleware for body parser
+app.use(express.json())
+app.use(express.urlencoded())
 const port = 3000
 
+// Endpoints to serve user interface
 app.get('/', (req, res) => {
     res.sendFile("/pages/homepage.html", {root: __dirname})
 })
@@ -11,6 +15,33 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/signup', (req, res) => {
+    res.sendFile("/pages/signup.html", {root: __dirname})
+})
+
+// Endpoints for APIs
+app.post('/getnotes', (req, res) => {
+    // Using json webtoken for user authentication
+    const {userToken} = req.body
+    res.sendFile("/pages/signup.html", {root: __dirname})
+})
+
+app.post('/login', (req, res) => {
+    const {userToken} = req.body
+    res.sendFile("/pages/signup.html", {root: __dirname})
+})
+
+app.post('/signup', (req, res) => {
+    const {userToken} = req.body
+    res.sendFile("/pages/signup.html", {root: __dirname})
+})
+
+app.post('/addnote', (req, res) => {
+    const {userToken} = req.body
+    res.sendFile("/pages/signup.html", {root: __dirname})
+})
+
+app.post('/deletenote', (req, res) => {
+    const {userToken} = req.body
     res.sendFile("/pages/signup.html", {root: __dirname})
 })
 
